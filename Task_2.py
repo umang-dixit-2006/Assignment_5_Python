e352218e18d2c2197146beb06d72df1a705c1d2a
Umang <212006umang@gmail.com>
2025-09-28 10:53:01 +0530

    Create Task_2.py

---
+++ Task_2.py
@@ -0,0 +1,6 @@
+OgList = [1, 2, 3, 4, 5, 6, 7, 8 , 9, 10]
+print(f"Original List: {OgList}")
+ExtList = OgList[:5]
+print(f"Extracted first five elements: {ExtList}")
+RevExtList = ExtList[::-1]
+print(f"Reversed extracted elements: {RevExtList}")
